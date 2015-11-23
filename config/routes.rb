24178ca3afff5344c9dfd41517b71af7866dece5
Ahmed El Masry <ahmed.elmasry707@gmail.com>
2015-11-23 13:04:44 +0200

    initial commit and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'pages/home'
+
   resources :articles do
     resources :comments
   end
  
   root 'welcome#index'
+
+  get '/home' , to: 'pages#home'
 
  
 end
