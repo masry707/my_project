d59c4eaf2620bda5bb21d8a75ff96c344a799e92
Ahmed El Masry <ahmed.elmasry707@gmail.com>
2015-11-23 15:18:37 +0200

    create chefs and recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123125904) do
+ActiveRecord::Schema.define(version: 20151123130943) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20151123125904) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string "username"
