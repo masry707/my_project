a3115a4e7a27c31b6da9f1e8d9d64dab2a2a612d
Ahmed El Masry <ahmed.elmasry707@gmail.com>
2015-11-27 17:29:04 +0200

    image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123151142) do
+ActiveRecord::Schema.define(version: 20151127144533) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151123151142) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
   create_table "users", force: true do |t|
