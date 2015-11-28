ce30f5b17d4460773143580f4091de65cf1d1eb5
masry707 <ahmed.elmasry707@icloud.com>
2015-11-28 12:27:40 +0000

    adding secure password to chefs and create register and edit pages

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'mini_magick', '3.8.0'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
 # gem 'unicorn'
---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,37 @@
+class ChefsController < ApplicationController
+   
+   def new
+       @chef = Chef.new
+   end
+   
+   def create
+       @chef = Chef.new(chef_params)
+       
+       if @chef.save
+          flash[:success] = "Your Account is created successfully!"
+          redirect_to recipes_path
+      else
+          render 'new'
+       end
+   end
+   
+   def edit
+       @chef = Chef.find(params[:id])
+   end
+   
+   def update
+       @chef = Chef.find(params[:id])
+       
+       if @chef.update(chef_params)
+           flash[:success] = "Your Account is updated successfully!"
+           redirect_to recipes_path
+       else
+          render 'edit' 
+       end
+   end
+   
+   private
+    def chef_params
+       params.require(:chef).permit(:chef_name, :email, :password) 
+    end
+end
